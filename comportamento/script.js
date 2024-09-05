b149c644ad43cc770d9c709632207cd9b3744478
Marcos-Petry <marcospetryjunior321@gmail.com>
2024-09-04 21:16:35 -0300

    Implementação pwa

--- comportamento/script.js
+++ comportamento/script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "componente/iconMenu/menu.svg";
+        document.querySelector('.icon').src = "componente/iconMenu/menu36px.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "componente/iconMenu/fecharMenu.svg";
+        document.querySelector('.icon').src = "componente/iconMenu/iconClose36px.png";
     }
 }
